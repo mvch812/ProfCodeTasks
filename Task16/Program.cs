858d7d0104ec72ef738079c99dc0e30d1c376510
MVCH812 <mvch812@gmail.com>
2021-11-10 22:06:57 +0300

    Выполнены все условия задачи

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -1,8 +1,8 @@
 ﻿// Задача 16
 // Сформировать случайным образом N-элементный массив, заполненный однозначными целыми числами разных знаков. Задать k и выяснить
-// •	сколько раз повторяется k в массиве
-// •	сколько элементов находится между первым и последним k
-// •	сколько раз встречается k
+// 1.1.	сколько раз повторяется k в массиве
+// 1.2.	сколько элементов находится между первым и последним k
+// 1.3	сколько раз встречается k
 
 void FillArrayA(int[] A)
 {
@@ -29,12 +29,14 @@ void PrintArray(int[] A) // Метод, печатающий массив A
 
 int[] A = new int[10];
 FillArrayA(A);
+Console.WriteLine();
+Console.WriteLine("Случайно сформированный массив:");
 PrintArray(A);
 Console.WriteLine();
 Console.WriteLine("Введите число k:");
 int k = Convert.ToInt32(Console.ReadLine());
 
-int countPowt(int[] A)
+int countK(int[] A)
 {
     int count = 0;
     for (int x = 0; x < A.Length; x++)
@@ -46,4 +48,50 @@ int countPowt(int[] A)
     }
     return count;
 }
-Console.WriteLine($"Число {k} повторяется {countPowt(A)} раз(а)");
+
+int FirstIndexK(int[] A)
+{
+    for (int x1 = 0; x1 < A.Length; x1++)
+    {
+        if (A[x1] == k)
+        {
+            return x1 + 1;
+        }
+    }
+    return 0;
+}
+
+int SecondIndexK(int[] A)
+{
+    for (int x2 = 0; x2 < A.Length; x2++)
+    {
+        if (A[A.Length - 1 - x2] == k)
+        {
+            return A.Length - x2;
+        }
+    }
+    return 0;
+}
+Console.WriteLine(SecondIndexK(A));
+Console.WriteLine(FirstIndexK(A));
+if (countK(A) >= 2)
+{
+    Console.WriteLine($"1.1.Число {k} повторяется  {countK(A) - 1} раз");
+    Console.WriteLine($"1.2.Между первым и последним числом k={k}  {Math.Abs(SecondIndexK(A) - FirstIndexK(A)) - 1} элементов");
+    Console.WriteLine($"1.3.Число {k} встречается {countK(A)} раз");
+}
+else
+{
+    if (countK(A) == 1)
+    {
+        Console.WriteLine($"1.1.Число {k} не повторяется в массиве.");
+        Console.WriteLine($"1.2.Число k={k} не повторяется в массиве");
+        Console.WriteLine($"1.3.Число {k} встречается {countK(A)} раз");
+    }
+    else
+    {
+        Console.WriteLine($"1.1.Число {k} не встречается в массиве.");
+        Console.WriteLine($"1.2.Число {k} не встречается в массиве.");
+        Console.WriteLine($"1.3.Число {k} не встречается в массиве.");
+    }
+}
