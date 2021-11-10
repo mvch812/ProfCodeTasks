989e8a9bbb6501c1338a4bff70899c3942f635e3
MVCH812 <mvch812@gmail.com>
2021-11-10 17:37:55 +0300

    Задача 16 решена

---
+++ Task16/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 16
+// Сформировать случайным образом N-элементный массив, заполненный однозначными целыми числами разных знаков. Задать k и выяснить
+// •	сколько раз повторяется k в массиве
+// •	сколько элементов находится между первым и последним k
+// •	сколько раз встречается k
+
+void FillArrayA(int[] A)
+{
+    int length = A.Length;
+    int index = 0;
+    while (index < length)
+    {
+        A[index] = new Random().Next(-10, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] A) // Метод, печатающий массив A  
+{
+    int index = 0;
+    while (index < A.Length)
+    {
+        {
+            Console.Write($"{A[index]} ");
+        }
+        index++;
+    }
+}
+
+int[] A = new int[10];
+FillArrayA(A);
+PrintArray(A);
+Console.WriteLine();
+Console.WriteLine("Введите число k:");
+int k = Convert.ToInt32(Console.ReadLine());
+
+int countPowt(int[] A)
+{
+    int count = 0;
+    for (int x = 0; x < A.Length; x++)
+    {
+        if (A[x] == k)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine($"Число {k} повторяется {countPowt(A)} раз(а)");
